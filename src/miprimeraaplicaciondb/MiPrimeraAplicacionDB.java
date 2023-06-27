432fff49ccc3b696976544e569f348c8478ce512
JX <>
2023-06-27 18:52:02 -0500

    Proyecto

---
+++ src/miprimeraaplicaciondb/MiPrimeraAplicacionDB.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package miprimeraaplicaciondb;
+
+import java.sql.ResultSet;
+
+/**
+ *
+ * @author Usuario
+ */
+public class MiPrimeraAplicacionDB {
+
+      
+    public static void main(String[] args){
+        FrmEstudiante fe=new FrmEstudiante();
+        fe.setVisible(true);
+        /*Scanner sc = new Scanner(System.in);
+        String _Cedula, _Nombre, _Apellido, _Observacion;
+        EstudianteBeans Est = new EstudianteBeans();
+        
+        System.out.println("Ingrese la cedula");
+        _Cedula = sc.next();
+        Est.setCedula(_Cedula);
+        
+        System.out.println("Ingrese el Nombre");
+        _Nombre = sc.next();
+        Est.setNombre(_Nombre);
+        
+        System.out.println("Ingrese el Apellido");
+        _Apellido = sc.next();
+        Est.setApellido(_Apellido);
+        
+        System.out.println("Ingrese la Observacion");
+        _Observacion = sc.next();
+        Est.setObservacion(_Observacion);
+        
+        Est.Insertar_Estudiante();
+        Est.Consultar_Estudiante();*/
+    }
+}
+
